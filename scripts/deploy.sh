f0b09e0d729a83ea7161b5cca7d337b12ad55c22
Gimpneek <colin@gimpneek.com>
2019-03-15 22:12:07 +0000

    - Using more secure password-stdin option for Docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ sudo apt-get update -y
 sudo apt-get install jq -y
 
 # or login DockerHub
-docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASSWORD
+echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
 
 # build the docker image and push to an image repository
 docker build -t $IMAGE_REPO_URL .
