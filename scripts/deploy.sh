890e808085d92a7149249b92e9470faa77abeecf
Gimpneek <colin@gimpneek.com>
2019-03-15 21:38:13 +0000

    - Starting to add auto-deploy script to automate deployments to ECS

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+# install AWS SDK
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# install necessary dependency for ecs-deploy
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+# or login DockerHub
+docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASSWORD
+
+# build the docker image and push to an image repository
+docker build -t $IMAGE_REPO_URL .
+docker tag $IMAGE_REPO_URL:latest $IMAGE_REPO_URL:latest
+docker push $IMAGE_REPO_URL:latest
+
+# update an AWS ECS service with the new image
+# ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
